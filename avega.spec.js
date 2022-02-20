c5df7756783748c369b639c88945bdf6f8b4e4fb
Jannis K <44637581+JannisK89@users.noreply.github.com>
2022-02-20 10:30:53 +0100

    Update avega.spec.js

--- avega.spec.js
+++ avega.spec.js
@@ -2,11 +2,10 @@
 describe('My first Cypresss test suite', () => { 
 
     // it är ett test och följer oftast namnsättningen "it does something when something happens"
-    // Testet börjar här
-    it('Tests the work flow of PlanMaker', () => {
+    // Testet börjar här, se till att all kod du skriver hamnar mellan klammerna  som öppnas och stängs av "it"
+    it('Finds a user when searching with ID', () => {
         
         // Hemsidan som ska testas: https://frosty-cori-7b6201.netlify.app
 
     })
-
  })
