45df2ce7da4eefb64f7b82c9ea23e2e75ba66124
Jannis K <44637581+JannisK89@users.noreply.github.com>
2022-02-20 10:24:13 +0100

    Create avega.spec.js

---
+++ avega.spec.js
@@ -0,0 +1,12 @@
+// Describe är en samling av tester
+describe('My first Cypresss test suite', () => { 
+
+    // it är ett test och följer oftast namnsättningen "it does something when something happens"
+    // Testet börjar här
+    it('Tests the work flow of PlanMaker', () => {
+        
+        // Hemsidan som ska testas: https://frosty-cori-7b6201.netlify.app
+
+    })
+
+ })
